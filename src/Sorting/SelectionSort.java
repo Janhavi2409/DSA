86b7e811e444218e6ffe55ca95c7812df0518114
PixelPioneers0 <pixelpioneers2023.official@gmail.com>
2024-01-09 10:24:25 +0530

    Sorting Techniques

--- src/Sorting/SelectionSort.java
+++ src/Sorting/SelectionSort.java
@@ -1,2 +1,26 @@
-package Sorting;public class SelectionSort {
+package Sorting;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void selectionSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[minIndex] > array[j]) {
+                    minIndex = j;
+                }
+            }
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {12, 56, -23, 6, 21, 68, -53, 78, 0, 25, -16};
+        System.out.println("Array before sorting: " + Arrays.toString(array));
+        selectionSort(array);
+        System.out.println("Array after sorting: " + Arrays.toString(array));
+    }
 }
