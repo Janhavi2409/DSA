2f542f4b5ce6d10543c14b57cc4c8c81b80c1ddb
PixelPioneers0 <pixelpioneers2023.official@gmail.com>
2024-01-09 11:12:23 +0530

    Sorting Techniques

--- src/Sorting/InsertionSort.java
+++ src/Sorting/InsertionSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class InsertionSort {
     public static void insertionSort(int[] array) {
-        for (int i = 0; i < array.length-1; i++) {
+        for (int i = 0; i < array.length - 1; i++) {
             for (int j = i + 1; j > 0; j--) {
                 if (array[j] < array[j - 1]) {
                     int temp = array[j];
